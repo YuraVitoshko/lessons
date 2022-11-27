1a0df5ab494c170a2bb15fa030c880ea0fdc52e0
Yurii <98182161+YuraVitoshko@users.noreply.github.com>
2022-11-27 13:40:27 +0200

    123

---
+++ lesson_22/js/script.js
@@ -0,0 +1,202 @@
+'use strict'
+
+// console.log('Я вчу JS');
+
+/*
+let myName = `Yura`,
+	myAge = 23,
+	myMessage = `Hello`;
+*/
+
+/*
+let myAge = 18;
+myAge = 23;
+console.log(myAge);
+*/
+
+/*
+let myAge = 18;
+let myNewAge = 23;
+myAge = myNewAge;
+console.log(myAge);
+*/
+
+/*
+const myAge = 23;
+console.log(myAge);
+*/
+
+/*
+const MY_AGE = 23;
+const myNewAge = 18 + 5;
+console.log(myNewAge);
+*/
+
+// let colorEyes
+
+// let user;
+// let userName;
+// userName = `Вася`;
+// user = userName;
+// console.log(user);
+
+/*
+if (true){
+	var size = 15;
+}
+console.log(size);
+*/
+
+/*
+let userName;
+console.log(typeof userName);
+console.log(userName);
+
+if ( userName === undefined ) {
+	console.log(`Змінна не визначена`);
+}
+if (typeof userName === `undefined` ) {
+	console.log(`Змінна не визначена`);
+}
+*/
+
+/*
+let block = document.querySelector(`.block`);
+console.log(block);
+*/
+
+/*
+let willYouMarryMe = true;
+if (willYouMarryMe) {
+	console.log(`:)`);
+} else {
+	console.log(`:(`);
+}
+*/
+
+/*
+let trueOrFalse = 58 > 20;
+console.log(trueOrFalse);
+*/
+
+/*
+let number = 20;
+console.log(number);
+console.log(typeof number);
+
+let getInfinity = -20 / 0;
+console.log(getInfinity);
+console.log(typeof getInfinity);
+*/
+
+/*
+let nun = `Hello` / 5;
+console.log(nun);
+console.log(typeof nun);
+*/
+
+/*
+const bigInteger = 111151561613206846168436165181n;
+console.log(typeof bigInteger);
+*/
+
+/*
+let userName = `Yura`;
+console.log(typeof userName);
+*/
+
+/*
+let userAge = 23;
+let userAgeInfo = `Вік: ${userAge}`;
+console.log(userAgeInfo);
+*/
+
+/*
+let userInfo = {
+	name: `Yura`,
+	age: 23
+}
+console.log(userInfo)
+console.log(typeof userInfo)
+*/
+
+
+/*
+let id = Symbol(`id`);
+console.log(typeof id)
+*/
+
+/*
+let funcVariable = function name(params){
+
+}
+console.log(typeof funcVariable)
+*/
+
+
+/*
+let userAge = 23;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = String(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+
+/*
+let userTrue = true;
+console.log(userTrue);
+console.log(typeof userTrue);
+
+userTrue = String(true);
+
+console.log(userTrue);
+console.log(typeof userTrue);
+*/
+
+/*
+let userAge = `23`;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+/*
+let userAge = `Yura`;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Number(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+/*
+let userAge = `46` / `2`;
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+/*
+let userAge = ``;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = Boolean(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+let userSize = `28` / `2`;
+console.log(typeof userSize);
+
+
